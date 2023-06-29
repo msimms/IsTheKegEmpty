4381492f458a9442b9a6ad6a059254a191b226c3
Mike Simms <msimms01@gmail.com>
2023-06-29 16:20:23 -0400

    Stubbed out the iOS/macOS app.

---
+++ src/apple/IsTheKegEmpty/IsTheKegEmptyApp.swift
@@ -0,0 +1,15 @@
+//
+//  IsTheKegEmptyApp.swift
+//  Created by Michael Simms on 6/29/23.
+//
+
+import SwiftUI
+
+@main
+struct IsTheKegEmptyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
