3968a609ab4adfb143eee20c295ef007648b30c7
Mike Simms <msimms01@gmail.com>
2024-04-01 12:44:33 -0500

    Switched to the embassy HAL.

--- src/sensor/src/main.rs
+++ src/sensor/src/main.rs
@@ -1,81 +1,21 @@
 #![no_std]
 #![no_main]
-extern crate panic_halt;
-extern crate embedded_hal;
-extern crate rp2040_hal;
+#![feature(type_alias_impl_trait)]
 
-// Ensure we halt the program on panic (if we don't mention this crate it won't
-// be linked)
-use panic_halt as _;
+use embassy_executor::Spawner;
+use embassy_rp::gpio::{Level, Output};
+use embassy_time::{Duration, Timer};
+use panic_probe as _;
 
-// Alias for our HAL crate
-use rp2040_hal as hal;
-
-// A shorter alias for the Peripheral Access Crate, which provides low-level
-// register access
-use hal::pac;
-
-// Some traits we need
-use embedded_hal::digital::v2::OutputPin;
-use rp2040_hal::clocks::Clock;
-
-/// The linker will place this boot block at the start of our program image. We
-/// need this to help the ROM bootloader get our code up and running.
-#[link_section = ".boot2"]
-#[used]
-pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_GENERIC_03H;
-
-/// External high-speed crystal on the Raspberry Pi Pico board is 12 MHz. Adjust
-/// if your board has a different frequency
-const XTAL_FREQ_HZ: u32 = 12_000_000u32;
-
-#[rp2040_hal::entry]
-fn main() -> ! {
-    // Grab our singleton objects
-    let mut pac = pac::Peripherals::take().unwrap();
-    let core = pac::CorePeripherals::take().unwrap();
-
-    // Set up the watchdog driver - needed by the clock setup code
-    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
-
-    // Configure the clocks
-    let clocks = hal::clocks::init_clocks_and_plls(
-        XTAL_FREQ_HZ,
-        pac.XOSC,
-        pac.CLOCKS,
-        pac.PLL_SYS,
-        pac.PLL_USB,
-        &mut pac.RESETS,
-        &mut watchdog,
-    )
-    .ok()
-    .unwrap();
-
-    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
-
-    // The single-cycle I/O block controls our GPIO pins
-    let sio = hal::Sio::new(pac.SIO);
-
-    // Set the pins to their default state
-    let pins = hal::gpio::Pins::new(
-        pac.IO_BANK0,
-        pac.PADS_BANK0,
-        sio.gpio_bank0,
-        &mut pac.RESETS,
-    );
-
-    // Configure the pins for reading the load cells.
-
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let peripherals = embassy_rp::init(Default::default());
+    let mut led = Output::new(peripherals.PIN_25, Level::Low);
     loop {
-        // Read from the first sensor.
+        led.set_high();
+        Timer::after(Duration::from_millis(50)).await;
 
-        // Read from the second sensor.
-
-        // Convert the readings to weight.
-
-        // Send to the web app.
-
-        // Wait.
-        delay.delay_ms(1000);
+        led.set_low();
+        Timer::after(Duration::from_millis(950)).await;
     }
 }
