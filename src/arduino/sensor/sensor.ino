39c828d2e9ad6646ba533aaebdbb71a7c5f056cc
Mike Simms <msimms01@gmail.com>
2024-07-02 13:53:18 -0400

    Cleanup

--- src/arduino/sensor/sensor.ino
+++ src/arduino/sensor/sensor.ino
@@ -1,34 +1,43 @@
 /*
-This example creates a client object that connects and transfers
-data using always SSL.
+  Web client
 
-It is compatible with the methods normally related to plain
-connections, like client.connect(host, port).
+ This sketch connects to a website (http://www.google.com)
+ using the WiFi module.
 
-Written by Arturo Guadalupi
-last revision November 2015
+ This example is written for a network using WPA encryption. For
+ WEP or WPA, change the WiFi.begin() call accordingly.
 
-*/
+ This example is written for a network using WPA encryption. For
+ WEP or WPA, change the WiFi.begin() call accordingly.
+
+ Circuit:
+ * Board with NINA module (Arduino MKR WiFi 1010, MKR VIDOR 4000 and Uno WiFi Rev.2)
+
+ created 13 July 2010
+ by dlf (Metodo2 srl)
+ modified 31 May 2012
+ by Tom Igoe
+ */
+
 
 #include <SPI.h>
-#include <WiFi101.h>
+#include <WiFiNINA.h>
 
 #include "arduino_secrets.h" 
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
-char ssid[] = SECRET_SSID;  // your network SSID (name)
-char pass[] = SECRET_PASS;  // your network password (use for WPA, or use as key for WEP)
-int keyIndex = 0;           // your network key Index number (needed only for WEP)
+char ssid[] = SECRET_SSID;        // your network SSID (name)
+char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;            // your network key index number (needed only for WEP)
 
 int status = WL_IDLE_STATUS;
 // if you don't want to use DNS (and reduce your sketch size)
 // use the numeric IP instead of the name for the server:
-// IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
+//IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
 char server[] = "www.google.com";    // name address for Google (using DNS)
 
-// Initialize the Ethernet client library
-// with the IP address and port of the server
+// Initialize the Ethernet client library with the IP address and port of the server
 // that you want to connect to (port 80 is default for HTTP):
-WiFiSSLClient client;
+WiFiClient client;
 
 void setup() {
   // Initialize serial and wait for port to open:
@@ -37,11 +46,16 @@ void setup() {
     ; // wait for serial port to connect. Needed for native USB port only
   }
 
-  // Check for the presence of the shield:
-  if (WiFi.status() == WL_NO_SHIELD) {
-    Serial.println("WiFi shield not present");
-    // don't continue:
+  // Check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
     while (true);
+  }
+
+  String fv = WiFi.firmwareVersion();
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+    Serial.println("Please upgrade the firmware");
   }
 
   // Attempt to connect to WiFi network:
@@ -51,16 +65,15 @@ void setup() {
     // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
     status = WiFi.begin(ssid, pass);
 
-    // wait 10 seconds for connection:
+    // Wait 10 seconds for connection:
     delay(10000);
   }
-  Serial.println("Connected to wifi");
-  printWiFiStatus();
+  Serial.println("Connected to WiFi");
+  printWifiStatus();
 
   Serial.println("\nStarting connection to server...");
- 
-  // if you get a connection, report back via serial:
-  if (client.connect(server, 443)) {
+  // If you get a connection, report back via serial:
+  if (client.connect(server, 80)) {
     Serial.println("connected to server");
     // Make a HTTP request:
     client.println("GET /search?q=arduino HTTP/1.1");
@@ -78,7 +91,7 @@ void loop() {
     Serial.write(c);
   }
 
-  // if the server's disconnected, stop the client:
+  // If the server's disconnected, stop the client:
   if (!client.connected()) {
     Serial.println();
     Serial.println("disconnecting from server.");
@@ -89,20 +102,19 @@ void loop() {
   }
 }
 
-
-void printWiFiStatus() {
-  // print the SSID of the network you're attached to:
+void printWifiStatus() {
+  // Print the SSID of the network you're attached to:
   Serial.print("SSID: ");
   Serial.println(WiFi.SSID());
 
-  // print your WiFi shield's IP address:
+  // Print your board's IP address:
   IPAddress ip = WiFi.localIP();
   Serial.print("IP Address: ");
   Serial.println(ip);
 
-  // print the received signal strength:
+  // Print the received signal strength:
   long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI):");
+  Serial.print("Signal strength (RSSI):");
   Serial.print(rssi);
   Serial.println(" dBm");
 }
